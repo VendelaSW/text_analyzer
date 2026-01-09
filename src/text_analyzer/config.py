10a5d0345c522e05b41ee16facc95554b3baf30c
Vendela <waltalex2@hotmail.com>
2026-01-09 19:33:04 +0100

    added more pathing to config

--- src/text_analyzer/config.py
+++ src/text_analyzer/config.py
@@ -3,3 +3,6 @@ from pathlib import Path
 PACKAGE_DIR = Path(__file__).parent
 PROJECT_ROOT = PACKAGE_DIR.parent
 
+# Optional placeholders for potential future folders
+DATA_DIR = PROJECT_ROOT / "data"
+OUTPUT_DIR = PROJECT_ROOT / "output"
